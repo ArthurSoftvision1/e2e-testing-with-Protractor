425942356e469f63d77afe5eba0ad5330adb1fc0
MolnarArthur <arthur.molnar@usource.ro>
2019-02-22 15:41:03 +0200

    e2e tests

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,28 @@
+import { AppPage } from './app.po';
+
+
+describe('ng5-create-routes App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display home page', () => {
+    page.navigateTo();
+    // browser.pause();
+    expect(page.getParagraphText()).toEqual('Home Page');
+  });
+
+  it('should display about button', () => {
+    page.navigateTo();
+    expect(page.getAboutButton().getText()).toEqual('About')
+  });
+
+  it('should route to about page', () => {
+    page.navigateTo();
+    page.getAboutButton().click();
+    // browser.pause();
+    expect(page.getAboutText()).toEqual('About Page')
+  });
+});
---
+++ e2e/app.po.ts
@@ -0,0 +1,19 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-home h2')).getText();
+  }
+
+  getAboutButton() {
+    return element(by.css('[routerlink="/about"]'));
+  }
+
+  getAboutText() {
+    return element(by.css('app-about h2')).getText();
+  }
+}
